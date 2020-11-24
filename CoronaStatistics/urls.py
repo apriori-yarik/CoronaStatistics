c794580c404ff571a5b41ee6cf7d66c1b68f1bd6
Yaroslav Dokuzov <yarik2803chess@yandex.ru>
2020-11-24 20:14:36 +0200

    media

--- CoronaStatistics/urls.py
+++ CoronaStatistics/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
     path('', include('django.contrib.auth.urls')),
 ]
 
-if settings.DEBUG:
-	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#if settings.DEBUG:
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
